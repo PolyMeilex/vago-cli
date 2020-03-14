734b280cef14d9f844dfa649e0850df9d080ecc4
Poly <bartek.marynczak@op.pl>
2020-03-14 20:59:08 +0100

    Init

---
+++ src/select.rs
@@ -0,0 +1,20 @@
+use dialoguer::{select, theme::ColorfulTheme};
+
+pub fn run(title: &str, data: &[String]) -> Option<usize> {
+    if data.len() == 0 {
+        return None;
+    }
+    let theme = ColorfulTheme::default();
+
+    let id = select::Select::with_theme(&theme)
+        .with_prompt(title)
+        .default(0)
+        .items(&data[..])
+        .interact();
+
+    if let Ok(id) = id {
+        Some(id)
+    } else {
+        None
+    }
+}
